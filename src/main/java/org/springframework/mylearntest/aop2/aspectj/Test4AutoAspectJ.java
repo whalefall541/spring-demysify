26dccdd6c6e672838a0acf987e2d2e36b8af5ce6
jack541 <jackchen541@sina.com>
2020-08-01 01:08:02 +0800

    2020-08-01 01:08:02

--- src/main/java/org/springframework/mylearntest/aop2/aspectj/Test4AutoAspectJ.java
+++ src/main/java/org/springframework/mylearntest/aop2/aspectj/Test4AutoAspectJ.java
@@ -5,13 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @Author: WhaleFall541
- * @Date: 2020/8/1 0:42
+ * @Date: 2020/8/1 0:43
+ *
  */
 public class Test4AutoAspectJ {
 	public static void main(String[] args) {
 //		ApplicationContext context = new ClassPathXmlApplicationContext("annotationawareaspectJautoproxycreator\\annotationawareaspectJautoproxycreator.xml");
 		ApplicationContext context = new ClassPathXmlApplicationContext("annotationawareaspectJautoproxycreator" +
 				"/annotationawareaspectJautoproxycreatorxsd.xml");
+		// todo
 		Object proxy = context.getBean("target");
 		((Foo)proxy).method1();
 		((Foo)proxy).method2();
